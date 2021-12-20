6769cef73fc030ed218cc6bdb684c8073e9a6a09
Akshath <akshathmahajan13@gmail.com>
2021-12-20 12:06:31 +0530

    Created accounts apps
    Added profile model
    Added placeholder blog models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    pass
+
+class BlogLike(models.Model):
+    pass
+
+class BlogComment(models.Model):
+    pass
+
