d19d9a0840403214a32bf3edaa49929d94a751f9
Akshath <akshathmahajan13@gmail.com>
2021-12-20 19:12:51 +0530

    Bug fixes and url mapping

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views_class import CreateBlog, UpdateBlog, DeleteBlog
+from .views import CreateComment, CreateLike, HomeView, BlogDetailView
 urlpatterns = [
+    path('', HomeView.as_view()),
+    path('<int:pk>/', BlogDetailView.as_view()),
     path('create/', CreateBlog.as_view(), name="create_blog"),
     path('update/<int:pk>/', UpdateBlog.as_view(), name="update_blog"),
     path('delete/<int:pk>/', DeleteBlog.as_view(), name="dreate_blog"),
+
+    # API:
+    path('<int:pk>/like/', CreateLike.as_view(), name="create_like"),
+    path('<int:pk>/comment/', CreateComment.as_view(), name="create_comment"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.views import ListView,DetailView
+from django.views.generic import ListView,DetailView
 from .models import BlogPost,BlogComment,BlogLike
-
+from django.views import View
 # Create your views here.
 class HomeView(ListView):
     model = BlogPost
-    template_name = 'index.html'
+    template_name = 'blog/index.html'
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {'posts': BlogPost.objects.filter(author=request.user)})
 
 class BlogDetailView(DetailView):
     model = BlogPost
-    template_name = 'post.html'
+    template_name = 'blog/post.html'
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'post': BlogPost.objects.get(id=self.kwargs['pk']), 'comments': BlogPost.comments.get(id=self.kwargs['pk']), 'likes': BlogPost.likes.get(id=self.kwargs['pk'])})
+        return render(request, self.template_name, {'post': BlogPost.objects.get(id=self.kwargs['pk']), 'comments': BlogPost.comments,  'likes': BlogPost.likes})
 
-
-class GetComments(DetailView):
-    model=BlogComment
-    template_name=''
 
 class GetLikes(DetailView):
     model=BlogLike
     template_name=''
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {'likes': BlogLike.objects.filter(blog__id=self.kwargs['pk'])})
-    def post(self, request, *args, **kwargs):
-        pass
-
-class CreateLike():
+class CreateLike(View):
     def post(self, request, pk):
         blog = BlogPost.objects.get(pk=pk)
         BlogLike(
@@ -38,7 +31,7 @@ class CreateLike():
         ).save()
         return JsonResponse({"status": "Success"})
 
-class CreateComment():
+class CreateComment(View):
     def post(self, request, pk):
         blog = BlogPost.objects.get(pk=pk)
         comment = BlogComment(
