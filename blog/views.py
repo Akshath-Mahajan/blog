fbc68967b0ce960d31e3176b63584990143a5e71
ADIthaker <56800392+ADIthaker@users.noreply.github.com>
2021-12-20 22:11:09 +0530

    added bg image

--- blog/views.py
+++ blog/views.py
@@ -31,7 +31,7 @@ class BlogDetailView(DetailView):
         blog = BlogPost.objects.get(id=self.kwargs['pk'])
         comments = BlogComment.objects.filter(blog=blog)
         likes = BlogLike.objects.filter(blog=blog)
-        return render(request, self.template_name, {'post': blog, 'comments': comments,  'likes': likes})
+        return render(request, self.template_name, {'post': blog, 'comments': comments,  'likes': likes, 'user': request.user})
 
 
 class UserView(ListView):
