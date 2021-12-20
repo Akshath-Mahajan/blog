29f3f84a9f8edf08c0b2e633651ef15de39da6da
AdithyaSanyal <adithyasanyal@gmail.com>
2021-12-20 17:30:19 +0530

    Like and comment create views

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.views import ListView,DetailView
 from .models import BlogPost,BlogComment,BlogLike
 
@@ -27,3 +28,22 @@ class GetLikes(DetailView):
         return render(request, self.template_name, {'likes': BlogLike.objects.filter(blog__id=self.kwargs['pk'])})
     def post(self, request, *args, **kwargs):
         pass
+
+class CreateLike():
+    def post(self, request, pk):
+        blog = BlogPost.objects.get(pk=pk)
+        BlogLike(
+            blog=blog,
+            like_author = request.user
+        ).save()
+        return JsonResponse({"status": "Success"})
+
+class CreateComment():
+    def post(self, request, pk):
+        blog = BlogPost.objects.get(pk=pk)
+        comment = BlogComment(
+            blog=blog,
+            comment_author = request.user,
+            comment_content = request.POST['content'])
+        comment.save()
+        return JsonResponse({"status": "Success","comment":comment})
