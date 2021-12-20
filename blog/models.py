c8694d053628625c343755827c66e813cc99bfa0
AdithyaSanyal <adithyasanyal@gmail.com>
2021-12-20 12:08:11 +0530

    Models creation

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    author=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title=models.CharField(max_length=200)
+    content=models.CharField(max_length=2000)
+    created_date=models.DateTimeField(auto_now_add=True)
+    
